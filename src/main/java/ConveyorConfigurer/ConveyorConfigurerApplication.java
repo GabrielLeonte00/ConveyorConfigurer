77bd0637fef4eea793f32c2e0bf1ed68cbfacc01
Gabriel Leonte <gabriel16.leonte@gmail.com>
2023-11-14 14:35:35 +0200

    Base project with excel creation model

---
+++ src/main/java/ConveyorConfigurer/ConveyorConfigurerApplication.java
@@ -0,0 +1,13 @@
+package ConveyorConfigurer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConveyorConfigurerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConveyorConfigurerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ConveyorConfigurer/ConveyorConfigurerApplicationTests.java
@@ -0,0 +1,13 @@
+package ConveyorConfigurer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConveyorConfigurerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
